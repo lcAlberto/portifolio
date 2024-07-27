b86dc33f3e5cb30ff748656ea89a48c3041b2855
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-07-27 16:01:17 -0300

    instalation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -19,6 +16,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <RouterView />
 </template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 
 <style scoped>
 header {
