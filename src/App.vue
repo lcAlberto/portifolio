81777ecc8133b76fc66b22cdc2f8b571abb1abf6
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-07-30 16:22:18 -0300

    fix deployment errors

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Navbar from '@/components/NavbarComponent.vue'
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
 </script>
 
 
