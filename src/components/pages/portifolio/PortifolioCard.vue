8d35eeb09fd724c8061513bda8c179dd7022ccf1
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-07-31 18:15:37 -0300

    Add repository projects page

--- src/components/pages/portifolio/PortifolioCard.vue
+++ src/components/pages/portifolio/PortifolioCard.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="card bg-secondary w-80 shadow-xl">
-    <div class="card-body">
-      <img :src="getImageUrl()" :alt="props.alt" class="w-11/12" />
-      <div class="card-actions justify-center">
+  <div class="card bg-img w-80 shadow-xl bg-center bg-contain h-72 group"
+       :style="`background-image: url('${getImageUrl()}')`">
+    <div class="card-body z-10 bg-opacity-0 hover:bg-opacity-80 bg-base-100">
+      <div class="hidden group-hover:flex flex-col text-end justify-end">
         <a
-          :href="props.link"
+          :href="link"
           target="_blank"
-          class="font-extrabold text-xl"
+          class="font-extrabold text-primary text-2xl"
         >
           {{ props.title }}
+          <font-awesome-icon :icon="['fas', 'arrow-up-right-from-square']" class="ml-2" />
         </a>
+        <p class="m-0">{{ props.subtitle }}</p>
       </div>
     </div>
   </div>
@@ -20,7 +22,8 @@ const props = defineProps({
   alt: { type: String, default: null },
   title: { type: String, default: null },
   description: { type: String, default: null },
-  link: { type: String, default: null }
+  link: { type: String, default: null },
+  subtitle: { type: String, default: null }
 })
 
 function getImageUrl() {
--- src/components/pages/portifolio/PortifolioView.vue
+++ src/components/pages/portifolio/PortifolioView.vue
@@ -4,32 +4,35 @@
       <h1 class="text-4xl font-bold mb-5">Portifólio</h1>
       <div class="mt-5 flex flex-wrap items-center justify-center gap-5 my-5">
         <portifolio-card
-          image-path="skills/GIT.svg"
+          image-path="img/projects/batcaverna.png"
           alt="vite-logo"
           link="https://github.com/lcAlberto/batcaverna"
           title="Batcaverna"
+          subtitle="Sistema que o Batman e a Liga da Justiça usam em suas missões!"
         />
         <portifolio-card
-          image-path="skills/GIT.svg"
+          image-path="img/projects/splash.png"
           alt="vite-logo"
           link="https://github.com/lcAlberto/Splash---English-School"
           title="Splash Escola de Inglês"
         />
         <portifolio-card
-          image-path="skills/GIT.svg"
+          image-path="img/projects/goldfinch.png"
           alt="vite-logo"
           link="https://github.com/lcAlberto/goldfinch"
           title="Goldfinch Landing Page"
+          subtitle="Plata forma de investimento, sob demanda"
         />
-        <portifolio-card
-          image-path="skills/GIT.svg"
-          alt="vite-logo"
-          title="Procriare"
-        />
+        <!--        <portifolio-card-->
+        <!--          image-path=""-->
+        <!--          alt="vite-logo"-->
+        <!--          title="Procriare"-->
+        <!--        />-->
       </div>
       <a
         href="https://github.com/lcAlberto/"
         data-tip="Perfil no Github"
+        target="_blank"
         class="btn btn-primary my-5 w-52 tooltip flex items-center"
       >
         Ver mais projetos
